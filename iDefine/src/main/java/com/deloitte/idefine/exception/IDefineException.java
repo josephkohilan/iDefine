4f32375d4d7eff7d3cc536174db98c5f659c561d
jmartinthiagaraj <jmartinthiagaraj@USBLRJMARTINTH1.us.deloitte.com>
2018-02-19 17:06:11 +0530

    Exception changes

---
+++ iDefine/src/main/java/com/deloitte/idefine/exception/IDefineException.java
@@ -0,0 +1,11 @@
+package com.deloitte.idefine.exception;
+
+public class IDefineException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public IDefineException(String exceptionMessage){
+		super(exceptionMessage);
+	}
+
+}
---
+++ iDefine/src/main/java/com/deloitte/idefine/utility/ExceptionConstants.java
@@ -0,0 +1,7 @@
+package com.deloitte.idefine.utility;
+
+public class ExceptionConstants {
+	
+	public static final String INVALID_INPUT_EXCEPTION = "Exception:100 - No or invaild input";
+
+}
